037834cbffec53eb9ac8be9cf341238749e19e8a
Arunprasat Selvaraj <smarun16@gmail.com>
2019-06-23 22:23:26 +0100

    Initial commit Created the framework.

---
+++ EncryptedDeviceKit/EncryptedDeviceKit.h
@@ -0,0 +1,19 @@
+//
+//  EncryptedDeviceKit.h
+//  EncryptedDeviceKit
+//
+//  Created by Arunprasat Selvaraj on 23/06/2019.
+//  Copyright © 2019 Arunprasat Selvaraj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EncryptedDeviceKit.
+FOUNDATION_EXPORT double EncryptedDeviceKitVersionNumber;
+
+//! Project version string for EncryptedDeviceKit.
+FOUNDATION_EXPORT const unsigned char EncryptedDeviceKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EncryptedDeviceKit/PublicHeader.h>
+
+
