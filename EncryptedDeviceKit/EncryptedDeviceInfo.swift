c2bec1f33e0af4d05c183da21169d60862ddbe24
Arunprasat Selvaraj <smarun16@gmail.com>
2019-06-24 13:45:44 +0100

    Removed the testing code for the encryption & decryption and removed the testing code.

--- EncryptedDeviceKit/EncryptedDeviceInfo.swift
+++ EncryptedDeviceKit/EncryptedDeviceInfo.swift
@@ -8,148 +8,43 @@
 
 import Foundation
 import UIKit
-import CommonCrypto
-import Security
 
-public protocol Encrypter {
+
+public struct Device {
     
-    func encryptedDeviceInfo() throws -> Data
-    func decrypt(_ encrypted: Data) throws -> Data
+    var name: String?
+    var model: String?
+    var localizedModel: String?
+    var systemName: String?
+    var systemVersion: String?
+    var batteryLevel: String?
+    var isBatteryMonitoringEnabled: Bool?
+    var isProximityMonitoringEnabled: Bool?
+    var isMultitaskingSupported: Bool?
+   
+    init(name: String, model: String, localizedModel: String, systemName: String, systemVersion: String, batteryLevel: Float, isBatteryMonitoringEnabled: Bool, isProximityMonitoringEnabled: Bool, isMultitaskingSupported: Bool) {
+        
+        self.name =  name
+        self.model = model
+        self.localizedModel = localizedModel
+        self.systemName = systemName
+        self.systemVersion = systemName
+        self.batteryLevel = String(batteryLevel)
+        self.isBatteryMonitoringEnabled =  isBatteryMonitoringEnabled
+        self.isProximityMonitoringEnabled = isProximityMonitoringEnabled
+        self.isMultitaskingSupported = isMultitaskingSupported
+    }
 }
-
 
 public class EncryptedDeviceInfo {
-    
-    //Constants
-    let name = "name"
-    let model = "model"
-    let localizedModel = "localizedModel"
-    let systemName = "systemName"
-    let systemVersion = "systemVersion"
-    let batteryLevel = "batteryLevel"
-    let proximityState = "proximityState"
-    
-    let isBatteryMonitoringEnabled = "isBatteryMonitoringEnabled"
-    let isProximityMonitoringEnabled = "isProximityMonitoringEnabled"
-    let isMultitaskingSupported = "isMultitaskingSupported"
-    
-    //Variables
-    private var key: Data
-    private var inputVector: Data
-    
-    //Init
-    public init(key: Data, iv: Data) throws {
-        guard !(key.isEmpty) else {
-            throw Error.badKeyLength
-        }
-        guard !(iv.isEmpty) else {
-            throw Error.badInputVectorLength(iv: iv)
-        }
-        self.key = key
-        self.inputVector = iv
-    }
-    
-    
-    public enum Error: Swift.Error {
-        case keyGeneration(status: Int)
-        case encryptFailed
-        case badKeyLength
-        case badInputVectorLength(iv: Data)
-    }
-}
 
-extension EncryptedDeviceInfo {
-    
-    //To get the device data in encryped type
-    //Call this methode in your class it will return the device in the encripted formate
-    open func getEncryptedCurrentDeviceData(completionBlock: (Data) -> Void, errorBlock: (Error) -> Void) {
-        
-        do {
-            //It returns the device data in encrypted formate.
-            let encrptedData = try self.encryptedDeviceInfo()
-            completionBlock(encrptedData)
-        } catch  {
-            errorBlock(Error.encryptFailed)
-        }
-    }
-    
-    //To decrypt pass the encrypted value to the EncryptedDeviceKit with Key & inputVector values which is used for the encrytion.
-    open func getDecryptedCurrentDeviceData(encrptedData:Data, completionBlock: (Data) -> Void, errorBlock: (Error) -> Void) {
-        
-        do {
-            //It returns the decrypted data for the given encrptedData
-            let decrptedData = try self.decrypt(encrptedData)
-            completionBlock(decrptedData)
-        } catch  {
-            errorBlock(Error.encryptFailed)
-        }
-    }
-    
-    
     //To get the current device Info
-    func getDeviceInfo() -> Data {
+    public func getDeviceInfo(completionHandler: (Device) -> Void) {
         
         let device  = UIDevice.current
         
-        var deviceInfo = [String: Any]()
+        let deviceModel = Device.init(name: device.name, model: device.model, localizedModel: device.localizedModel, systemName: device.systemName, systemVersion: device.systemVersion, batteryLevel: device.batteryLevel, isBatteryMonitoringEnabled: device.isBatteryMonitoringEnabled, isProximityMonitoringEnabled: device.isProximityMonitoringEnabled, isMultitaskingSupported: device.isMultitaskingSupported)
         
-        deviceInfo[name] = device.name
-        deviceInfo[model] = device.model
-        deviceInfo[localizedModel] = device.localizedModel
-        deviceInfo[systemName] = device.systemName
-        deviceInfo[systemVersion] = device.systemVersion
-        deviceInfo[isBatteryMonitoringEnabled] = device.isBatteryMonitoringEnabled
-        deviceInfo[batteryLevel] = device.batteryLevel
-        deviceInfo[isProximityMonitoringEnabled] = device.isProximityMonitoringEnabled
-        
-        do {
-            let jsonData = try JSONSerialization.data(withJSONObject: deviceInfo, options: [])
-            return jsonData
-        } catch {
-            return Data()
-        }
-    }
-    
-    // Used to encrypt or decrypt the data based on the key & inputVector values
-    // This will called for the Encrypt protocal function for encrypt or decrypt
-    private func crypt(input: Data, operation: CCOperation) throws -> Data {
-        
-        let outputLength = input.count + kCCBlockSizeAES128
-        var outputBuffer = Array<UInt8>(repeating: 0,
-                                        count: outputLength)
-        var numBytesEncrypted = 0
-        
-        let status = CCCrypt(CCOperation(kCCEncrypt), // operation
-            CCAlgorithm(kCCAlgorithmAES),   // algorithm
-            CCOptions(kCCOptionPKCS7Padding), // options
-            Array(key),  // key
-            kCCKeySizeAES256, // keylength
-            Array(inputVector), // iv
-            Array(input), // dataIn
-            input.count, // dataInLength
-            &outputBuffer, // dataOut
-            outputLength, // dataOutLength
-            &numBytesEncrypted) // dataOutMoved
-        
-        guard status == kCCSuccess else {
-            throw Error.encryptFailed
-        }
-        
-        let outputBytes = outputBuffer.prefix(numBytesEncrypted)
-        return Data(outputBytes)
-    }
-}
-
-
-extension EncryptedDeviceInfo: Encrypter {
-    
-    public func encryptedDeviceInfo() throws -> Data {
-        
-        return try crypt(input: self.getDeviceInfo(), operation: CCOperation(kCCEncrypt))
-    }
-    
-    public func decrypt(_ encrypted: Data) throws -> Data {
-        
-        return try crypt(input: encrypted, operation: CCOperation(kCCDecrypt))
+        completionHandler(deviceModel)
     }
 }
